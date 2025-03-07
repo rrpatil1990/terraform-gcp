b18f7684b6cbaa064ea39fdd8118c89d37471d83
rrpatil1990 <rrpatil129@gmail.com>
2025-03-08 01:45:14 +0800

    updated provider details

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = "D:\\Study\\practice\\terraform\\gcp\\terraform-key.json"
+  # credentials = "D:\\Study\\practice\\terraform\\gcp\\terraform-key.json"
   project = "formal-office-452901-e9"
   zone = "asia-southeast1-a"
 }
